65a5215a24c9552771b24c07ea5de5daaa7fc594
MutasimRafid <mrafid1998@gmail.com>
2021-11-03 20:29:29 +0600

    35 added

---
+++ 35_Beautiful Year.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int year;
+cin>>year;
+
+
+while(true){
+        year += 1;
+    int a = year/1000;
+    int b = year/100%10;
+    int c = year/10%10;
+    int d = year%10;
+    if(a!=b && a!=c && a!=d && b!=c && b!=d && c!=d){
+    cout<<year<<endl;
+     break;
+    }
+}
+
+return 0;
+}
