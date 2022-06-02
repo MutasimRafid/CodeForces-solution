feff7eeec08ed892791e015603739482ee2fb83a
MutasimRafid <mrafid1998@gmail.com>
2022-06-02 20:19:36 +0600

    800 rating

---
+++ Equidistant Letters 1626 A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+void solve(){
+ string s;
+ cin>>s;
+ sort(s.begin(),s.end());
+ cout<<s<<endl;
+
+}
+
+int main(){
+ll t;
+cin>>t;
+while(t--){
+    solve();
+}
+
+return 0;
+}
+
+
